5489c49b89b2e59e28a6a096fcae655e0766540e
gusmojsma <gusmojsma@student.gu.se>
2023-11-22 16:40:55 +0100

    #1 Changes to schemas

--- booking-service/models/booking.js
+++ booking-service/models/booking.js
@@ -5,8 +5,8 @@ var bookingSchema = new Schema({
     date: { type: Date },
     start: {type: String },
     end: { type: String },
-    patient: {},
-    dentist: {},
+    patient: { type: Schema.Types.String, ref: 'patients', required: true },
+    dentist: { type: Schema.Types.String, ref: 'dentists', required: true },
 });
 
 module.exports = mongoose.model('bookings', bookingSchema);
--- booking-service/models/clinic.js
+++ booking-service/models/clinic.js
@@ -5,7 +5,7 @@ var clinicSchema = new Schema({
     name: { type: String, unique: true, required: true },
     address: { type: String },
     openHours: { type: String },
-    dentists: {}
+    dentists: [{ type: Schema.Types.ObjectId, ref: 'dentist'}]
 });
 
 module.exports = mongoose.model('clinics', clinicSchema);
