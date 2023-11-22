94e1f01754e28fa6988ad70fb770bead357531e7
Marcelo <gussanma@student.gu.se>
2023-11-22 12:27:07 +0100

    #1 app.js configured

---
+++ booking-service/app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var path = require('path');
+var cors = require('cors');
+
+const { MongoClient } = require("mongodb");
+const password = encodeURIComponent("iloveteeth");
+
+var mongoURI = process.env.MONGODB_URI || `mongodb+srv://admin:${password}@toothfixcluster0.ouccgbu.mongodb.net/?retryWrites=true&w=majority`
+var port = process.env.PORT || 4000;
+
+mongoose.connect(mongoURI).catch(function (err) {
+    if (err) {
+        console.error(`Failed to connect to MongoDB with given URI`);
+        console.error(err.stack);
+        process.exit(1);
+    }
+    console.log(`Connected to MongoDB with URI: ${mongoURI}`);
+});
+
+// Create Express app
+var app = express();
+// Parse requests of content-type 'application/json'
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// HTTP request logger
+app.use(morgan('dev'));
+// Enable cross-origin resource sharing for frontend must be registered before api
+app.options('*', cors());
+app.use(cors());
+
+// Define routes
+app.get('/api', function (req, res) {
+    res.json({ message: 'Welcome to ToothFix API' });
+});
+
+//put the routes:
+
+//catch invalid routes
+app.use('/api/*', function (req, res) {
+    res.status(404).send({ url: req.originalUrl + ' not found' })
+});
+
+// Error handler (i.e., when exception is thrown) must be registered last
+var env = app.get('env');
+// eslint-disable-next-line no-unused-vars
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    var err_res = {
+        'message': err.message,
+        'error': {}
+    };
+    if (env === 'development') {
+        // Return sensitive stack trace only in dev mode
+        err_res['error'] = err.stack;
+    }
+    res.status(err.status || 500);
+    res.json(err_res);
+});
+
+app.listen(port, function (err) { 
+    if (err) throw err;
+    console.log(`Express server listening on port ${port}, in ${env} mode`);
+    console.log(`Backend: http://localhost:${port}/api/`);
+});
