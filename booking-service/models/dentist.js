5e99bfe3f14074c9c7d883792d5b82a7c128aced
Andrii Demchenko <demch.andrew@gmail.com>
2023-11-30 16:40:08 +0100

    fix. added clinic to the dentist model and changed clinic model

--- booking-service/models/dentist.js
+++ booking-service/models/dentist.js
@@ -5,7 +5,8 @@ var dentistSchema = new Schema({
     personnummer: { type: String, unique: true, required: true },
     name: { type: String },
     email: { type: String },
-    phoneNumber: { type: String }
+    phoneNumber: { type: String },
+    clinic: {type: Schema.Types.ObjectId, ref: 'clinic'}
 });
 
 module.exports = mongoose.model('dentists', dentistSchema);
