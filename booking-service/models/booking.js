9e294821d942bc753f2461f706141a00152121c1
Marcelo <marsant@student.chalmers.se>
2023-12-14 14:03:45 +0100

    #19 Edited schemas

--- booking-service/models/booking.js
+++ booking-service/models/booking.js
@@ -3,9 +3,8 @@ const Schema = mongoose.Schema;
 
 var bookingSchema = new Schema({
     date: { type: Date, required: true},
-    start: {type: String, required: true },
-    end: { type: String, required: true},
-    patient: { type: Schema.Types.String, ref: 'patients', required: true },
+    patientId: { type: String, required: true},
+    patientName: { type: String, required: true},
     patientEmail: { type: String, required: true},
     dentist: { type: Schema.Types.String, ref: 'dentists', required: true }, 
     slotId: { type: String },
