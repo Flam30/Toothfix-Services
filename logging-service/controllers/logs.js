3a0c9480692fb12e9ab65c87d90992cecd68a456
Marcelo <marsant@student.chalmers.se>
2024-01-08 21:18:22 +0100

    Fixed formatting for pipeline

--- logging-service/controllers/logs.js
+++ logging-service/controllers/logs.js
@@ -1,47 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Log = require('../models/log');
+var Log = require("../models/log");
 
 //POST
 router.post("/", async function (req, res, next) {
-    try {
-        const log = await Log.create(req.body);
-        res.status(201).json(log); 
-    } catch (error) {
-        return next(error);
-    }
+  try {
+    const log = await Log.create(req.body);
+    res.status(201).json(log);
+  } catch (error) {
+    return next(error);
+  }
 });
 
-//GET 
-router.get("/", async function (req, res, next) { 
-    try {
-        const logs = await Log.find({});
-        res.status(200).json(logs);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+//GET
+router.get("/", async function (req, res, next) {
+  try {
+    const logs = await Log.find({});
+    res.status(200).json(logs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 //GET specific
 router.get("/:id", async function (req, res) {
-    try {
-        var id = req.params.id;
-        const log = await Log.findOne({_id:id});
-        res.status(200).json(log);
-    } catch (error) {
-        res.status(404).json({ message: "Log not found" });
-    }
+  try {
+    var id = req.params.id;
+    const log = await Log.findOne({ _id: id });
+    res.status(200).json(log);
+  } catch (error) {
+    res.status(404).json({ message: "Log not found" });
+  }
 });
 
 //DELETE
 router.delete("/:id", async function (req, res) {
-    try {
-        var id = req.params.id;
-        await Log.deleteOne({_id:id});
-        res.status(200).json({ message: "Log deleted" });
-    } catch (error) {
-        res.status(404).json({ message: "Log not found" });
-    }
+  try {
+    var id = req.params.id;
+    await Log.deleteOne({ _id: id });
+    res.status(200).json({ message: "Log deleted" });
+  } catch (error) {
+    res.status(404).json({ message: "Log not found" });
+  }
 });
 
 module.exports = router;
--- logging-service/models/log.js
+++ logging-service/models/log.js
@@ -1,11 +1,11 @@
 //for bookings made
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var logSchema = new Schema({
-    eventType: { type: String, required: true },
-    eventDate: { type: String, required: true },
-    responsibleService: { type: String, required: true },
+  eventType: { type: String, required: true },
+  eventDate: { type: String, required: true },
+  responsibleService: { type: String, required: true },
 });
 
-module.exports = mongoose.model('log', logSchema);
+module.exports = mongoose.model("log", logSchema);
